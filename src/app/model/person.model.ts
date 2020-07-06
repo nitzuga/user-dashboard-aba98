23705bec966742572981c3cb64a859d5b56bc075
Agustín González Fernández <awugonzalez@gmail.com>
2020-07-06 00:56:01 -0400

    person model with defined restrictions

---
+++ src/app/model/person.model.ts
@@ -0,0 +1,31 @@
+export interface Person {
+    /**
+     * @description
+     * Identification number used in Chile, with valid verification digit. Required.
+     */
+    rut: string; // valid rut with verification digit, required
+
+    /**
+     * @description
+     * First name of the person. Max 50 characters. Required.
+     */
+    name: string; // max 50 char, required
+
+    /**
+     * @description
+     * Last name of the person. Max 60 characters. Required
+     */
+    lastname: string; // max 60 char, required
+
+    /**
+     * @description
+     * Age of the person. Must be over 18.
+     */
+    age: number; // > 18
+
+    /**
+     * @description
+     * Max 400 characters. Multiline.
+     */
+    address: string; // max 400 char, multiline
+}
